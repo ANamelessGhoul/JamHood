fd9bd3668848471ef345d36f01dd1264062345b8
egekol <egekol@hotmail.com>
2020-03-01 16:46:26 +0300

    ...

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -13,7 +13,7 @@ public class EnemyFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _animator = GetComponentInChildren<Animator>()
+        _animator = GetComponentInChildren<Animator>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
--- Assets/Scripts/ProceduralMesh.cs
+++ Assets/Scripts/ProceduralMesh.cs
@@ -10,8 +10,8 @@ public class ProceduralMesh : MonoBehaviour
     private Mesh mesh;
     private Vector3[] vertices;
     private int[] triangles;
-    private Vector2[] uv;
-    [SerializeField] private MeshRenderer meshR;
+    //private Vector2[] uv;
+    //[SerializeField] private MeshRenderer meshR;
     [SerializeField] private Vector3 vec1 = new Vector3(0, 0, 0);
     private void Awake()
     {
@@ -29,7 +29,7 @@ public class ProceduralMesh : MonoBehaviour
     {
         vertices = new Vector3[]{ vec1, new Vector3(0,0,1), new Vector3(1,0,0), new Vector3(1,0,1) };
         triangles = new int[] {0, 1, 2, 2, 1, 3};
-        uv = new Vector2[]{ new Vector2(0,0), new Vector2(1,0), new Vector2(0,1), new Vector2(1, 1) };
+        //uv = new Vector2[]{ new Vector2(0,0), new Vector2(1,0), new Vector2(0,1), new Vector2(1, 1) };
     }
 
     void CreateMesh()
@@ -37,6 +37,6 @@ public class ProceduralMesh : MonoBehaviour
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
-        mesh.uv = uv;
+        //mesh.uv = uv;
     }
 }
